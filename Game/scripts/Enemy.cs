389ba681a4ba1d7f81a5376f1567d3f806862737
Rasey24 <fede.gaudino@hotmail.com>
2020-05-10 19:58:55 -0300

    Enemy

---
+++ Game/scripts/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.scripts
+{
+    public class Enemy
+    {
+        string image = "";
+        private float x;
+        private float y;
+        private float scaleX;
+        private float scaleY;
+        private float angle;
+        private float offsetX;
+        private float offsetY;
+    }
+}
