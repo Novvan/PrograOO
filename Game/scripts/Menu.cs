9debbf24132a6c69e1866cd4b1f81cb4aef5adbd
igeier <ianallengeier@gmail.com>
2020-05-11 16:29:50 -0300

    Revert "Parte de herencias (GameObject)"
    This reverts commit 3e87d2fbb96cfbbd9d26d33ad1ac9ee125c79bdb.

--- Game/scripts/Menu.cs
+++ Game/scripts/Menu.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace Game.scripts
 {
-    public abstract class GameObject
+    public class Menu 
     {
+
     }
 }
--- Game/scripts/player.cs
+++ Game/scripts/player.cs
@@ -6,28 +6,30 @@ using System.Threading.Tasks;
 
 namespace Game.scripts
 {
-    public class Player : GameObject
+    public class Player
     {
-        //private float angle = 0;
-        //private float scale = 0.5f;
-        // private string texturePath = "textures/assets/Player/player.png";
         private float x;
-        private float y;           
+        private float y;
+        private Vector2 position;
         private LifeController lifeController;
-        private float speed = 300f;                   
+        private float speed = 300f;
+        private float angle = 0;
+        private float scale = 0.5f;
+        private Texture texture;
+        private string texturePath = "textures/assets/Player/player.png";
         private SpawnPoint spawnPoint;
-        
-        
 
 
+        public float Width => texture.Width * scale;
+        public float Height => texture.Height * scale;
+        public float OffsetX => Width / 2;
+        public float OffsetY => Height / 2;
 
         public LifeController LifeController
         {
             get => lifeController;
             set => lifeController = value;
         }
-
-        
 
         public float X
         {
@@ -41,10 +43,14 @@ namespace Game.scripts
             set => y = value;
         }
 
-       
-        public Player(Vector2 initialPosition, string texturePath, float angle, float scaleX, float scaleY, float speed) : base(initialPosition, texturePath, angle, scaleX, scaleY)
+        public Player(float initialX, float initialY, float angle)
         {
-            lifeController = new LifeController(100);  
+            x = initialX;
+            y = initialY;
+
+            texture = Engine.GetTexture(texturePath);
+
+            lifeController = new LifeController(100);
         }
 
         public void AssignSpawnpoint(SpawnPoint newSpawnpoint)
@@ -54,41 +60,39 @@ namespace Game.scripts
 
         public void MoveRight()
         {
-           position . x += speed * Program.deltaTime;
-            
+            x += speed * Program.deltaTime;
+            angle = 0f;
             //Engine.Debug(angle);
         }
 
         public void MoveLeft()
         {
-            position . x -= speed * Program.deltaTime;
-           
+            x -= speed * Program.deltaTime;
+            angle = 180f;
             //Engine.Debug(angle);
         }
 
         public void MoveUp()
         {
-            position . y -= speed * Program.deltaTime;
-           
+            y -= speed * Program.deltaTime;
+            angle = 270f;
             //Engine.Debug(angle);
         }
 
         public void MoveDown()
         {
-            position . y += speed * Program.deltaTime;
-            
+            y += speed * Program.deltaTime;
+            angle = 90f;
             //Engine.Debug(angle);
         }
 
-        //Override es para tener algo propio del player (clase hijo)
-        public override void Update()
+        public void Update()
         {
         }
 
-        public override void Render()
+        public void Render()
         {
-            //Base.Render es para heredar el render de la clase padre
-            base.Render();    
+            Engine.Draw(texture, x, y, scale, scale, angle, OffsetX, OffsetY);
         }
     }
-}    
+}
