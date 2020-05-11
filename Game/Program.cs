22a794bf0baefd92117b0663530af2cf736e8e50
NicoCoello <coellonicolas97@gmail.com>
2020-05-11 18:51:08 -0300

    Level

--- Game/Program.cs
+++ Game/Program.cs
@@ -6,13 +6,7 @@ namespace Game
 {
     public class Program
     {
-        public static float deltaTime;
-        static DateTime startTime;
-        static float lastFrameTime;
-        static Player player;
-        static int width = 1600;
-        static int height = 900;
-        private static Vector2 SpawnPoint;
+      
 
         static void Main(string[] args)
         {
@@ -20,8 +14,7 @@ namespace Game
 
             while (true)
             {
-                CalculateDeltaTime();
-                Input();
+                
                 Update();
                 Render();
             }
@@ -29,42 +22,12 @@ namespace Game
 
         static void Init()
         {
-            Engine.Initialize("BoxHead", width, height, false);
-
-
-            SpawnPoint = new Vector2(5, 5);
-            player = new Player(SpawnPoint, "textures/assets/Player/player.png",0,0.5f,0.5f,300f);
-            startTime = DateTime.Now;
+            Engine.Initialize();
+            Time.Initialize();
+            GameManager.Instance.Initialize();             
         }
 
-        private static void Input()
-        {
-            if (Engine.GetKey(Keys.D))
-            {
-                player.MoveRight();
-            }
-
-            if (Engine.GetKey(Keys.A))
-            {
-                player.MoveLeft();
-            }
-
-            if (Engine.GetKey(Keys.S))
-            {
-                player.MoveDown();
-            }
-
-            if (Engine.GetKey(Keys.W))
-            {
-                player.MoveUp();
-            }
-
-            if (Engine.GetKey(Keys.Q))
-            {
-                player.LifeController.GetDamage(1);
-                Engine.Debug(player.LifeController.CurrentLife);
-            }
-        }
+   
 
         private static void Update()
         {
@@ -72,23 +35,15 @@ namespace Game
             {
                 GameManager.Instance.NewWave();
             }*/
+            Time.Update();
+            GameManager.Instance.Update();
         }
-
         private static void Render()
         {
             Engine.Clear();
-            Engine.Draw("textures/assets/Map.png");
-            player.Render();
-
+            GameManager.Instance.Render();
             Engine.Show();
         }
-
-        static void CalculateDeltaTime()
-        {
-            float currentTime = (float)(DateTime.Now - startTime).TotalSeconds;
-            deltaTime = currentTime - lastFrameTime;
-            lastFrameTime = currentTime;
-            //Engine.Debug("DeltaTime:" + deltaTime);
-        }
+        
     }
 }
---
+++ Game/scripts/Level.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.scripts
+{
+    public class Level
+    {
+        public float deltaTime;
+        DateTime startTime;
+        float lastFrameTime;
+        Player player;
+        int width = 1600;
+        int height = 900;
+        private Vector2 SpawnPoint;
+
+        //en el constructor inicializamos todos los objetos del nivel
+        public Level()
+        {
+            SpawnPoint = new Vector2(5, 5);
+            player = new Player(SpawnPoint, "textures/assets/Player/player.png",0,0.5f,0.5f,300f);
+            startTime = DateTime.Now;
+        }
+
+        private void Input()
+        {
+            if (Engine.GetKey(Keys.D))
+            {
+                player.MoveRight();
+            }
+
+            if (Engine.GetKey(Keys.A))
+            {
+                player.MoveLeft();
+            }
+
+            if (Engine.GetKey(Keys.S))
+            {
+                player.MoveDown();
+            }
+
+            if (Engine.GetKey(Keys.W))
+            {
+                player.MoveUp();
+            }
+
+            if (Engine.GetKey(Keys.Q))
+            {
+                player.LifeController.GetDamage(1);
+                Engine.Debug(player.LifeController.CurrentLife);
+            }
+        }
+        public void Update()
+        {
+
+        }
+        public void Render()
+        {
+            Engine.Clear();
+            Engine.Draw("textures/assets/Map.png");
+            player.Render();
+
+            Engine.Show();
+        }
+
+         void CalculateDeltaTime()
+        {
+            float currentTime = (float)(DateTime.Now - startTime).TotalSeconds;
+            deltaTime = currentTime - lastFrameTime;
+            lastFrameTime = currentTime;
+            //Engine.Debug("DeltaTime:" + deltaTime);
+        }
+
+    }
+
+
+}
+    
+
