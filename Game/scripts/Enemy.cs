8925ff557725e6ac30a5896af9d2251976faca8a
igeier <ianallengeier@gmail.com>
2020-05-11 02:13:30 -0300

    enemy and regular enemy

--- Game/scripts/Enemy.cs
+++ Game/scripts/Enemy.cs
@@ -8,8 +8,35 @@ namespace Game.scripts
 {
     public class Enemy
     {
-        private LifeController lifeController;
+        private float _enemX;
+        private float _enemY;
+        private float _enemAngle;
+        private Texture _texture;
+        private string _texturePath;
+        private float _scale;
+        private LifeController _lifeController;
 
-        //public Enemy();
+        private float _width => _texture.Width * _scale;
+        private float _height => _texture.Height * _scale;
+        private float _offsetX => _width / 2;
+        private float _offsetY => _height / 2;
+
+        public LifeController EnemyLifeController
+        {
+            get => _lifeController;
+            set => _lifeController = value;
+        }
+
+        public float EnemX
+        {
+            get => _enemX;
+            set => _enemX = value;
+        }
+
+        public float EnemY
+        {
+            get => _enemY;
+            set => _enemY = value;
+        }
     }
 }
